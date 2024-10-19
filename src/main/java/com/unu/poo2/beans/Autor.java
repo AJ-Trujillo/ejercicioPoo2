9203111adcf8b87b2659401c165719876382fb71
Aj-Trujillo <jordintrujillo789@gmail.com>
2024-10-18 19:05:56 -0500

    primer comit del proyecto

---
+++ src/main/java/com/unu/poo2/beans/Autor.java
@@ -0,0 +1,46 @@
+package com.unu.poo2.beans;
+
+public class Autor {
+
+	private int idAutor;
+
+	public Autor() {
+		super();
+
+	}
+
+	public Autor(int idAutor, String nacionalidad, String nombreAutor) {
+		super();
+		this.idAutor = idAutor;
+		this.nacionalidad = nacionalidad;
+		this.nombreAutor = nombreAutor;
+	}
+
+	public int getIdAutor() {
+		return idAutor;
+	}
+
+	public void setIdAutor(int idAutor) {
+		this.idAutor = idAutor;
+	}
+
+	public String getNacionalidad() {
+		return nacionalidad;
+	}
+
+	public void setNacionalidad(String nacionalidad) {
+		this.nacionalidad = nacionalidad;
+	}
+
+	public String getNombreAutor() {
+		return nombreAutor;
+	}
+
+	public void setNombreAutor(String nombreAutor) {
+		this.nombreAutor = nombreAutor;
+	}
+
+	private String nacionalidad;
+	private String nombreAutor;
+
+}
